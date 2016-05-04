097068b1d290a37cbb5b3b50f7a5cd6730e3fce2
A_Lippok <alippok@hotmail.co.uk>
2016-05-04 13:55:09 +0100

    Adds rest of Exotic Statuses to valueCalulator

--- src/main/java/com/codeclan/al/ValueCalculator.java
+++ src/main/java/com/codeclan/al/ValueCalculator.java
@@ -12,6 +12,18 @@ public class ValueCalculator {
 			animal.setMarketPrice(1500.00);
 			return true;
 		}
+		if(exoticStatus.equals("rare")){
+			animal.setMarketPrice(1000.00);
+			return true;
+		}
+		if(exoticStatus.equals("uncommon")){
+			animal.setMarketPrice(600.00);
+			return true;
+		}
+		if(exoticStatus.equals("common")){
+			animal.setMarketPrice(300.00);
+			return true;
+		}
 		
 		throw new NullPointerException();
 		
